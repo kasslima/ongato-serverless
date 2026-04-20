beffa3dda4687ad932246ba619b26eeccde2d764
Lucas Ribeiro <lucasribeiro292004@hotmail.com>
2026-04-20 13:35:34 -0300

    feat(validation): add Zod schemas for ID and query parameters

---
+++ src/shared/validation/schema.ts
@@ -0,0 +1,15 @@
+import z from "zod";
+
+//id
+export const idParamSchema = z.object({
+  id: z.coerce.number()
+    .int({ message: "O parâmetro precisa ser um número inteiro" })
+    .positive({ message: "O número precisa ser maior de 0" })
+})
+
+//query
+export const QuerySchema = z.object({
+  page: z.coerce.number().int().positive().default(1),
+  limit: z.coerce.number().int().positive().max(100).default(10)
+})
+export type QuerySchemaDTO = z.infer<typeof QuerySchema>
