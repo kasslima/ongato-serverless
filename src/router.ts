733fb1cee2a8ee5f1003f88952971e7fd31e0a27
Lucas Ribeiro <lucasribeiro292004@hotmail.com>
2026-04-25 09:14:08 -0300

    refactor: -rename features/banners to features/banner

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,7 @@ import { authsRoutes } from "./features/auth/routes";
 import { usersRoutes } from "./features/users/routes";
 import { docsRoutes } from "./features/docs/routes";
 import { Env } from "./shared/type";
-import { bannersRoutes } from "./features/banners/routes";
+import { bannersRoutes } from "./features/banner/routes";
 
 //import { petsRoutes } from "./features/pets/routes";
 
